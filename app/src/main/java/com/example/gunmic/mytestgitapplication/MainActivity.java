5aa83f50abac916af13eed9916dcadcffb9a4c56
Migu3lle <gundi.m@hotmail.com>
2018-03-20 13:06:58 +0100

    Initial commit

---
+++ app/src/main/java/com/example/gunmic/mytestgitapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.gunmic.mytestgitapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("Ich bin ein Test");
+        Log.d("test_log", "Hallo ich bin ein Test Logeintrag");
+    }
+}
