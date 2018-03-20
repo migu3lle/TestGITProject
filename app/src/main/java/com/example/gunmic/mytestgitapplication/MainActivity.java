f8d645f980b95615dd0fd1605f9c5cbd6f38ef9c
Migu3lle <gundi.m@hotmail.com>
2018-03-20 13:20:29 +0100

    Initial commit

--- app/src/main/java/com/example/gunmic/mytestgitapplication/MainActivity.java
+++ app/src/main/java/com/example/gunmic/mytestgitapplication/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
 
         System.out.println("Ich bin ein Test");
         Log.d("test_log", "Hallo ich bin ein Test Logeintrag");
+        Log.d("test_log", "Hallo ich bin ein neuer Test Logeintrag");
     }
 }
